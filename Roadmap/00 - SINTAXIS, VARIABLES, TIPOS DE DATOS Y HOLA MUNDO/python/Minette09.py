9129a7b7b20354252c1e2661fe322cb310ba7a21
Minette09 <99312464+Minette09@users.noreply.github.com>
2024-08-23 16:26:26 -0600

    "#00 - Phyton"

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Minette09.py
@@ -0,0 +1,28 @@
+#https://python.org
+
+"esto es un comentario"
+
+nombre = "Pamela"
+edad = "28"
+
+HORAS_DIA =24
+
+Primitivos = "soy un texto"
+Entero = "2024"
+x = True 
+y = False
+
+print('¡Hola Phyton!')
+
+
+
+
+
+
+
+
+
+
+
+
+
